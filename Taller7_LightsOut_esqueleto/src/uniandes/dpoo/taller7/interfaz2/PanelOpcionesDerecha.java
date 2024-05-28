af78cef493ba385119d5e01a4490e309ccbcd1b5
Angie Gutiérrez Trujillo <ac.gutierrezt1@uniandes.edu.co>
2024-05-28 08:10:50 -0500

    cambio

--- Taller7_LightsOut_esqueleto/src/uniandes/dpoo/taller7/interfaz2/PanelOpcionesDerecha.java
+++ Taller7_LightsOut_esqueleto/src/uniandes/dpoo/taller7/interfaz2/PanelOpcionesDerecha.java
@@ -1,8 +1,8 @@
 package uniandes.dpoo.taller7.interfaz2;
 
 
-import java.awt.FlowLayout;
-
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -16,18 +16,28 @@ public class PanelOpcionesDerecha extends JPanel{
 
 	
 	public PanelOpcionesDerecha() {
+        BoxLayout box_l= new BoxLayout(this, BoxLayout.Y_AXIS); 
+        setLayout(box_l); //Ayuda a organizar los elementos en el panel de arriba a abajo
 		
 		botonNuevo = new JButton("NUEVO");
 		botonReiniciar = new JButton("REINICIAR");
 		botonTop = new JButton("TOP-10");
         botonCambiarJugador = new JButton("CAMBIAR JUGADOR");
         
+		//centrar los botones en el panel
+		botonNuevo.setAlignmentX(CENTER_ALIGNMENT);
+		botonReiniciar.setAlignmentX(CENTER_ALIGNMENT);
+		botonTop.setAlignmentX(CENTER_ALIGNMENT);
+		botonCambiarJugador.setAlignmentX(CENTER_ALIGNMENT);
+
+		add(Box.createVerticalStrut(50)); //Espacio en blanco de 50 pixeles
 		add(botonNuevo);
+		add(Box.createVerticalStrut(20)); //Espacio entre los botones (20 pixeles)
 		add(botonReiniciar);
+		add(Box.createVerticalStrut(20));
 		add(botonTop);
+		add(Box.createVerticalStrut(20));
         add(botonCambiarJugador);
 		
-        FlowLayout fl = new FlowLayout(); //Ayuda a organizar los elementos en el panel de arriba a abajo}
-        setLayout(fl);
 	}
 }
